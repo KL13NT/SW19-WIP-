aa61278c911fa0c3ac14cdc2a24627d12aab1c06
Nabil <nabil.tharwat@outlook.com>
2018-12-27 19:01:56 +0200

    90%

--- index.js
+++ index.js
@@ -4,19 +4,12 @@ import './GLOBAL_CSS/globalstyle.styl'
 // import { BrowserRouter, Route, NavLink } from 'react-router-dom'
 const MainDisplay1 = (props)=>{
     if(props.page ==1 || props.page ==2) return (
-        <>
-            <div id="paper2" className="npaper2"></div>
-            <div id="paper1" className="npaper1"></div>
-            <p id="paper1Text">{props.messageWritten}</p>
-        </>
+            <p id="paper1Text" className='paper1Text'>{props.messageWritten}</p>
     )
     else return (
-        <>
-            <p id="paper1Text">Stay with me in <span id="year">{props.year}</span></p>
-        </>
+            <p id="paper1Text" className = 'paper1Text'>Stay with me in <span id="year">{props.messageWritten}</span></p>
     )
 }
-
 class MainDisplay extends Component{
     state={
         page: 1, 
@@ -27,14 +20,19 @@ class MainDisplay extends Component{
         year: '____'
     }
     componentDidMount(){
-        let paper1Side = document.getElementById('paper1').offsetLeft 
-        let paper1Text = document.getElementById('paper1Text')
+        
+        this.paper1 = document.getElementById('paper1')
+        this.paper2 = document.getElementById('paper2')
+        this.paper1Text = document.getElementById('paper1Text')
+        this.container = document.getElementById('container')
+        this.topMarginRoot = document.getElementById('topMarginRoot')
+        this.paper1Side = document.getElementById('paper1').offsetLeft 
 
-        paper1Text.style.left = `${paper1Side}px` //set text to be on top of paper 
-        paper1Text.style.top = `${document.getElementById('paper1').offsetTop + 100}px` //set text to be on top of paper
+        this.paper1Text.style.left = `${this.paper1Side}px` //set text to be on top of paper 
+        this.paper1Text.style.top = `${this.paper1.offsetTop + 100}px` //set text to be on top of paper
         
         this.moveBackground(10)
-        this.writeText(0)
+        this.writeText(0, 35)
     }
     moveBackground = (x)=>{
         if(x == 10){
@@ -50,7 +48,7 @@ class MainDisplay extends Component{
             this.moveBackground(10)
         } 
     }
-    writeText = (x)=>{
+    writeText = (x,y)=>{
         let i = 0 
         let viewUpdate = []
         let writingInterval = setInterval(()=>{ //TEXT WRITING ANIMATION
@@ -64,7 +62,7 @@ class MainDisplay extends Component{
                 clearInterval(writingInterval)
                 if(x == 0) this.flicker()
             }
-        }, 35) //WRITING ANIMATION SPEED
+        }, y) //WRITING ANIMATION SPEED
     }
     flicker = ()=>{
         let scoreAppearance = true
@@ -92,63 +90,102 @@ class MainDisplay extends Component{
             }
         }, 200)//FLICKERING ANIMATION SPEED
     }
-    removeText = (x)=>{
+    removeText = (x,y)=>{
+        let defaultMsg = [...this.state.messageWritten]
         let textRemovalInterval = setInterval(()=>{
-            let defaultMsg = [...this.state.messageWritten]
-            let dump = defaultMsg.splice(defaultMsg.length -1, 1) //dumped value, not used
-            
             this.setState({messageWritten: [...defaultMsg]})
-            
+            let dump = defaultMsg.splice(defaultMsg.length -1, 1) //dumped value, not used
             if(this.state.messageWritten.length == 0){ 
                 clearInterval(textRemovalInterval)
                 if(x == 0) this.straightenPapers()
             }
-        }, 25) //TEXT REMOVAL ANIMATION SPEED
+        }, y) //TEXT REMOVAL ANIMATION SPEED
     }
     straightenPapers = ()=>{
-        paper2.classList.add('straightened')
+        this.paper2.classList.add('straightened')
         setTimeout(this.slideToRight, 500)
     }
     slideToRight = ()=>{
-        paper2.classList.add('slidRight')
+        this.paper2.classList.add('slidRight')
         container.style.backgroundColor = '#272727'
         
         setTimeout(()=>{
-            paper1.classList.add('slidRight')
+            this.paper1.classList.add('slidRight')
             topMarginRoot.classList.add('slidRight')
             this.moveBackground(20)
         }, 50) //TIME FOR MOVEMENT START
         setTimeout(this.transformPapers, 800)
     }
     transformPapers = ()=>{
-        paper2.classList.add('ndDisplayYell')
-        paper1.classList.add('ndDisplay')
-        topMarginRoot.style.color = '#ffffff'
-        paper1Text.style.left = '10%'
+        this.paper2.classList.add('ndDisplayYell')
+        this.paper1.classList.add('ndDisplay')
+        this.topMarginRoot.style.color = '#ffffff'
+        this.paper1Text.style.left = '10%'
         
         setTimeout(this.getDisplayBack, 2000)
     }
     getDisplayBack = ()=>{
-        paper1Text.style.marginLeft = topMarginRoot.style.marginLeft = 0 
-        paper1.classList.remove('slidRight')
-        paper2.classList.remove('slidRight')
-        paper1.classList.add('ndDisplayAll')
-        paper2.classList.add('ndDisplayAll')
-        paper1Text.classList.add('display2Font')
+        this.paper1Text.style.marginLeft = this.topMarginRoot.style.marginLeft = 0 
+        this.paper1.classList.remove('slidRight')
+        this.paper2.classList.remove('slidRight')
+        this.paper1.classList.add('ndDisplayAll')
+        this.paper2.classList.add('ndDisplayAll')
+        this.paper1Text.classList.add('display2Font')
         
         this.setState({
             message: 'A year just passed and our friendship is only getting stronger_'
         })
         setTimeout(()=>{
-            paper1Text.classList.add('yellowTopBorder');
-            this.writeText(1); 
+            this.paper1Text.classList.add('yellowTopBorder');
+            this.writeText(1, 35); 
             document.getElementById('canvas').style.visibility = 'visible';
         }, 400)
+        setTimeout(()=>{
+            
+            this.setState({
+                message: ''
+            })
+            this.removeText(1, 1)
+        }, 5000)
+        setTimeout(()=>{
+            this.scaleUp()
+        }, 7000)
+        setTimeout(()=>{this.fadeIn()}, 9000)
     }
-    
+    scaleUp = ()=>{
+        this.paper2.style.setProperty('visibility', 'hidden')
+        this.paper1.style.setProperty('transform', 'scale(2)')
+        this.paper1Text.style.setProperty('transformOrigin', 'center')
+        canvas.style.setProperty('display', 'none')
+        this.paper1Text.style.setProperty('transform', 'scale(20)')
+        setTimeout(()=>{this.paper1Text.style.setProperty('visibility', 'hidden')}, 300)
+        this.paper1Text.style.setProperty('marginLeft', 'auto')
+        this.paper1Text.style.setProperty('left', '0')
+        this.paper1Text.style.setProperty('marginLeft', 'auto')
+        this.paper1Text.style.setProperty('maxWidth', 'auto')
+        this.setState({message: '____'})
+    }
+    fadeIn = ()=>{
+        this.setState({page: 3})
+        this.paper1Text.classList.remove('yellowTopBorder')
+        this.paper1Text.style.setProperty('fontFamily', 'mainDisplayFont2')
+        this.paper1Text.style.setProperty('visibility', 'visible')
+        this.container.style.setProperty('background', 'white')
+        this.paper1Text.classList.add('centeredDisplay')
+        this.paper1Text.style.setProperty('transform', 'scale(1)')
+        this.setState({message: '2019'})
+        setTimeout(()=>{this.writeText(2, 50)} , 500)
+    }
+    /*
+        USE MAIN ENGINE TO POWER UP ALL FUNCTIONS 
+    */
     render(){
         return(
-            <MainDisplay1 messageWritten={this.state.messageWritten} page={this.state.page} year={this.state.year}/>
+            <>
+                <div id="paper2" className="npaper2"></div>
+                <div id="paper1" className="npaper1"></div>
+                <MainDisplay1 messageWritten={this.state.messageWritten} page={this.state.page} year={this.state.year}/>
+            </>
         )
     }
 }
@@ -156,7 +193,7 @@ class MainDisplay extends Component{
 ReactDOM.render(<MainDisplay/>, document.getElementById('mainDisplayRoot'))
 
     var COLORS, Confetti, NUM_CONFETTI, PI_2, canvas, confetti, context, drawCircle, drawCircle2, drawCircle3, i, range, xpos;
-    NUM_CONFETTI = 30;
+    NUM_CONFETTI = 20;
     COLORS = [
       [235, 90, 70],
       [97, 189, 79],
